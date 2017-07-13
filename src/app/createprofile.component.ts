51010d912b792d387e3e449cc6dd668a437da01d
Biju <bijukkutty@gmail.com>
2017-07-13 18:47:24 +0530

    LetsCreate Angular Routing Solved

--- src/app/createprofile.component.ts
+++ src/app/createprofile.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'my-app',
   templateUrl: './createprofile.component.html',
   styleUrls: ['./createprofile.component.css']
 })
 
  export class CreateProfileComponent  {
   title = 'Tour of Heroes';
+
+constructor( private router: Router) {
+}
+
+public redirectToProfileCreation(): void {
+	this.router.navigate(['./profileFields']);
+}
 }
